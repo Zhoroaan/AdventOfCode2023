3152bb7a4e10928dd35b265b8bcd6943e9b30d97
jbernhardsson <johan@johanbernhardsson.se>
2023-12-08 07:45:19 +0100

    Day 8 second star :)

---
+++ Day08/8.py
@@ -0,0 +1,39 @@
+from math import lcm
+
+with open("Input.txt") as fl:
+    directions, sp, *nodes = fl.read().strip().split("\n")
+
+directions = list(directions)
+nodes = [node.split(" = ") for node in nodes]
+
+
+nodes = {start: end[1:-1].split(", ") for start, end in nodes}
+
+
+current_node = "AAA"
+steps = 0
+while current_node != "ZZZ":
+    for direc in directions:
+        current_node = nodes[current_node][0 if direc == "L" else 1]
+        steps += 1
+        if current_node == "ZZZ":
+            break
+
+
+steps_list = []
+for current_node in nodes.keys():
+    if current_node.endswith("A"):
+        step2 = 0
+        while current_node.endswith("Z") is not True:
+            for direc in directions:
+                current_node = nodes[current_node][0 if direc == "L" else 1]
+                step2 += 1
+                if current_node.endswith("Z") is True:
+                    break
+        if step2 != 0:
+            steps_list.append(step2)
+
+print(steps_list)
+
+print(f"Number of steps taken: {steps}")
+print(f"Number of steps taken for all nodes: {lcm(*steps_list)}")
--- Day08/Day08.cpp
+++ Day08/Day08.cpp
@@ -2,10 +2,25 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <numeric>
 std::string Filename = "TestInput.txt";
 //std::string Filename = "Input.txt";
 
-int main(int argc, char* argv[])
+uint64_t LCM(const std::vector<uint64_t>& InNumbers)
+{
+    if (InNumbers.empty())
+        return 0;
+
+    if (InNumbers.size() == 1)
+        return InNumbers[0];
+
+    uint64_t result = InNumbers[0];
+    for (uint64_t i = 1; i < InNumbers.size(); i++)
+        result = std::lcm(result, InNumbers[i]);
+    return result;
+}
+
+int main(int /*argc*/, char* /*argv*/[])
 {
     std::ifstream inputFile;
     inputFile.open(Filename);
@@ -14,37 +29,44 @@ int main(int argc, char* argv[])
     std::getline(inputFile, inputLine);
     std::string instructionSequence = inputLine;
     std::getline(inputFile, inputLine); // skip the empty line
-    std::unordered_map<std::string, std::pair<std::string,std::string>> movementMapping;
+    std::unordered_map<std::string, std::pair<std::string, std::string>> movementMapping;
     while (inputFile.is_open() && !inputFile.eof())
     {
         std::getline(inputFile, inputLine);
         
         char from[4], left[4], right[4];
-        int result = sscanf_s(inputLine.c_str(),"%3s = (%3s, %3s)",
-            from, static_cast<unsigned>(_countof(from)),
-            left, static_cast<unsigned>(_countof(left)),
-            right, static_cast<unsigned>(_countof(right)));
+        sscanf_s(inputLine.c_str(),"%3s = (%3s, %3s)",
+                 from, static_cast<unsigned>(_countof(from)),
+                 left, static_cast<unsigned>(_countof(left)),
+                 right, static_cast<unsigned>(_countof(right)));
         movementMapping[from] = std::make_pair(left, right);
     }
 
-    std::string currentEntry = "AAA";
-    for (int64_t steps = 1; ;steps++)
+    std::vector<uint64_t> stepCount;
+    // These steps are cyclical so we only need to know when they all align
+    for (const auto& movement : movementMapping)
     {
-        if (steps % 100000000 == 0)
-            std::cout << "Calculating at: " << steps << std::endl;
-        if (instructionSequence[(steps - 1) % instructionSequence.length()] == 'L')
-            currentEntry = movementMapping[currentEntry].first;
-        else
-            currentEntry = movementMapping[currentEntry].second;
+        if (movement.first[2] != 'A')
+            continue;
 
-        if (currentEntry == "ZZZ")
+        std::string node = movement.first;
+
+        for (int64_t steps = 1; ;steps++)
         {
-            std::cout << "Day 8 Part 1: " << steps << " steps needed" << std::endl;
+            if (instructionSequence[(steps - 1) % instructionSequence.length()] == 'L')
+                node = movementMapping[node].first;
+            else
+                node = movementMapping[node].second;
+
+            if (node[2] != 'Z')
+                continue;
+
+            stepCount.push_back(steps);
             break;
         }
     }
 
-    //std::cout << "Day 8" << std::endl;
+    std::cout << "Result: " << LCM(stepCount) << std::endl;
     
     return 0;
 }
