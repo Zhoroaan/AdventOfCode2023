ff18fdbd7731e6a9eda00ee9a5bbc6a336d26009
jbernhardsson <johan@johanbernhardsson.se>
2023-12-12 21:30:59 +0100

    Day 12 second star (long run time brute force) :)

--- Day12/Day12.cpp
+++ Day12/Day12.cpp
@@ -1,13 +1,13 @@
-
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <execution>
 std::string Filename = "TestInput.txt";
 //std::string Filename = "Input.txt";
 
-bool IsValidInput(const std::string& InInputData, const std::vector<int64_t>& InGroups)
+bool IsValidInput(const std::string& InInputData, const std::vector<int64_t>& InGroups, int64_t& OutBrokeAtIndex)
 {
     bool isInGroup = false;
     int64_t currentGroupSize = 0;
@@ -15,6 +15,7 @@ bool IsValidInput(const std::string& InInputData, const std::vector<int64_t>& In
     //std::cout << "Testing permutation: " << InInputData << std::endl;
     for (int a = 0; a < InInputData.size(); ++a)
     {
+        OutBrokeAtIndex = a;
         if (isInGroup && (InInputData[a] == '.' || InInputData[a] == '?'))
         {
             isInGroup = false;
@@ -35,6 +36,7 @@ bool IsValidInput(const std::string& InInputData, const std::vector<int64_t>& In
 
         if (a == static_cast<int64_t>(InInputData.size()) - 1 && !isInGroup)
         {
+            OutBrokeAtIndex = -1;
             return true;
         }
     }
@@ -44,19 +46,25 @@ bool IsValidInput(const std::string& InInputData, const std::vector<int64_t>& In
     if (InGroups[currentGroupIndex] != currentGroupSize)
         return false;
     
+    OutBrokeAtIndex = -1;
     return true;
 }
 
-int64_t CountNumberOfPermutations(const std::string& InInputData, const std::vector<int64_t>& InGroups, int64_t InTotalNumberMissingSprings, int64_t InMinIndex = 0)
+int64_t CountNumberOfPermutations(std::vector<std::string>& bufferData, const std::string& InInputData, const std::vector<int64_t>& InGroups, int64_t InTotalNumberMissingSprings, int64_t& OutBrokeAt, int64_t InMinIndex = 0, int64_t InDepth = 0)
 {
     int64_t numPermutations = 0;
-    if (InTotalNumberMissingSprings == 0 && IsValidInput(InInputData, InGroups))
+    if (InTotalNumberMissingSprings == 0 && IsValidInput(InInputData, InGroups, OutBrokeAt))
     {
         //std::cout << "Matching permutation: " << InInputData << std::endl;
         return 1;
     }
+    else if (InTotalNumberMissingSprings <= 0)
+        return 0;
     
-    std::string workData;
+    if (bufferData.size() <= InDepth)
+        bufferData.emplace_back();
+
+    std::string& workData = bufferData[InDepth];
     for (int a = InMinIndex; a < InInputData.size(); ++a)
     {
         if (InInputData[a] != '?')
@@ -64,41 +72,88 @@ int64_t CountNumberOfPermutations(const std::string& InInputData, const std::vec
 
         workData = InInputData;
         workData[a] = '#';
-        numPermutations += CountNumberOfPermutations(workData, InGroups, InTotalNumberMissingSprings - 1, a + 1);
+        numPermutations += CountNumberOfPermutations(bufferData, workData, InGroups, InTotalNumberMissingSprings - 1, OutBrokeAt, a + 1, InDepth + 1);
+        if (OutBrokeAt != -1 && a >= OutBrokeAt)
+            return numPermutations;
     }
     return numPermutations;
 }
+
+struct RowInput
+{
+    std::string FullString;
+    std::string SpringCondition;
+    std::vector<int64_t> FullNumberGroup;
+    int64_t TotalNumberOfBrokenSprings = 0;
+};
 
 int main(int argc, char* argv[])
 {
     std::ifstream inputFile;
     inputFile.open(Filename);
     std::string inputLine;
-    int64_t totalSumPermutations = 0;
+    std::vector<RowInput> rowInputs;
     while (inputFile.is_open() && !inputFile.eof())
     {
-        bool b = IsValidInput(".?#..#?...###.", {1,1,3});
         std::getline(inputFile, inputLine);
         std::stringstream ss(inputLine);
-        std::string springCondition;
-        std::getline(ss, springCondition, ' ');
+        RowInput rowInput;
+        std::getline(ss, rowInput.SpringCondition, ' ');
+        
+        for (int a = 0; a < 5; ++a)
+        {
+            rowInput.FullString += rowInput.SpringCondition;
+            if (a != 4)
+                rowInput.FullString += '?';
+        }
 
         std::vector<int64_t> numberGroups;
-        int64_t totalNumberOfBrokenSprings = 0;
         for (int64_t group = -1; ss >> group;)
         {
             numberGroups.push_back(group);
             ss.ignore(1, ',');
-            totalNumberOfBrokenSprings += group;
+            rowInput.TotalNumberOfBrokenSprings += group;
         }
-        for (auto spring : springCondition)
+        for (int a = 0; a < 5; ++a)
+        {
+            for (auto count : numberGroups)
+            {
+                rowInput.FullNumberGroup.push_back(count);
+            }
+        }
+
+        rowInput.TotalNumberOfBrokenSprings *= 5;
+        
+        for (auto spring : rowInput.FullString)
         {
             if (spring == '#')
-                totalNumberOfBrokenSprings--;
+                rowInput.TotalNumberOfBrokenSprings--;
         }
-        int64_t numPermutations = CountNumberOfPermutations(springCondition, numberGroups, totalNumberOfBrokenSprings);
-        totalSumPermutations += numPermutations;
+        rowInputs.push_back(rowInput);
     }
+
+    std::atomic_int64_t totalSumPermutations = 0;
+    std::atomic_int64_t numFinished = 0;
+    /*std::for_each(
+    std::execution::par_unseq,
+    rowInputs.begin(),
+    rowInputs.end(),
+    [&totalSumPermutations](auto&& rowInput)*/
+    #pragma omp parallel for
+    for (int i = 0; i  < rowInputs.size(); ++i)
+    {
+        auto& rowInput = rowInputs[i];
+        std::vector<std::string> stringBuffer;
+        stringBuffer.reserve(256);
+        //do stuff with item
+        std::cout << "Working on [" << i + 1 << "/" << rowInputs.size() << "] " << rowInput.SpringCondition << std::endl;
+        int64_t outBrokeAt = -1;
+        int64_t numPermutations = CountNumberOfPermutations(stringBuffer, rowInput.FullString, rowInput.FullNumberGroup, rowInput.TotalNumberOfBrokenSprings, outBrokeAt);
+    
+        totalSumPermutations += numPermutations;
+        numFinished+= 1;
+        std::cout << "Finished " << numFinished << std::endl;
+    }//);
 
     std::cout << "Day 12 result: " << totalSumPermutations << std::endl;
     
